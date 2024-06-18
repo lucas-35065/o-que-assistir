9b070b54285bfefa0dfda5f97b39cf7e495e38da
Lucas Rodrigues <lucas.rodrigues.35065@gmail.com>
2024-06-18 00:58:19 +0000

    modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 function Home () {
-    return <h1>Geovana, você é o amor da minha vida!
-    Minha vida é maravilhosa, e atribuo MUITO disso a você.
-    Quero casar, viver minha vida do seu lado, e te amar, muito mais a cada dia.
-    Minha sorte maior é ter você, ser com você, estar com você e juntos sermos nós.
-    Escolher você todos os dias é a maior graça que alguém pode ter.
-    Então você nem sabe o tamanho da minha felicidade em ter sido escolhido por você, para sempre te escolher
-    Eu te amo, meu benzinho. Meu diamante rosa. Meu ouro branco. Minha paixão. Amor da minha vida.
+    return <h1>Geovana, você é o amor da minha vida! <br></br>
+    Minha vida é maravilhosa, e atribuo MUITO disso a você.<br></br>
+    Quero casar, viver minha vida do seu lado, e te amar, muito mais a cada dia.<br></br>
+    Minha sorte maior é ter você, ser com você, estar com você e juntos sermos nós.<br></br>
+    Escolher você todos os dias é a maior graça que alguém pode ter.<br></br>
+    Então, você nem sabe o tamanho da minha felicidade em ter sido escolhido por você, para sempre te escolher<br></br>
+    Eu te amo, meu benzinho. Meu diamante rosa. Meu ouro branco. Minha paixão. Amor da minha vida.<br></br>
     Hoje, aceitas casar comigo de novo?</h1>
 }
 
