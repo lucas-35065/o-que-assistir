762df1b1c83e83886e14c15c32f5f0d8aaaae8c0
Lucas Rodrigues <lucas.rodrigues.35065@gmail.com>
2024-06-18 00:45:55 +0000

    adicionando uma mensagem mais específica ao projeto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home () {
-    return <h1>O que assistir?</h1>
+    return <h1>Geovana, você é o amor da minha vida!
+    Minha vida é maravilhosa, e atribuo MUITO disso a você.
+    Quero casar, viver minha vida do seu lado, e te amar, muito mais a cada dia.
+    Minha sorte maior é ter você, ser com você, estar com você e juntos sermos nós.
+    Escolher você todos os dias é a maior graça que alguém pode ter.
+    Então você nem sabe o tamanho da minha felicidade em ter sido escolhido por você, para sempre te escolher
+    Eu te amo, meu benzinho. Meu diamante rosa. Meu ouro branco. Minha paixão. Amor da minha vida.
+    Hoje, aceitas casar comigo de novo?</h1>
 }
 
 export default Home;
