b983190f062603be4a43bba275a8d48dd758ca75
Lucas Rodrigues <lucas.rodrigues.35065@gmail.com>
2024-06-20 02:19:52 +0000

    estilizando porcamente a frase

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
+import styles from "./styles.module.css";
+
 function Home () {
-    return <h1>Geovana, você é o amor da minha vida! <br></br>
-    Minha vida é maravilhosa, e atribuo MUITO disso a você.<br></br>
+    return <h1 className={styles.title}>Geovana, você é o amor da minha vida! <br></br>
+    Minha vida é maravilhosa, e atribuo <span className={styles.detail}>MUITO</span> disso a você.<br></br>
     Quero casar, viver minha vida do seu lado, e te amar, muito mais a cada dia.<br></br>
-    Minha sorte maior é ter você, ser com você, estar com você e juntos sermos nós.<br></br>
+    <span className={styles.detail}>Minha sorte maior é ter você, ser com você, estar com você e juntos sermos nós.</span><br></br>
     Escolher você todos os dias é a maior graça que alguém pode ter.<br></br>
     Então, você nem sabe o tamanho da minha felicidade em ter sido escolhido por você, para sempre te escolher<br></br>
     Eu te amo, meu benzinho. Meu diamante rosa. Meu ouro branco. Minha paixão. Amor da minha vida.<br></br>
-    Hoje, aceitas casar comigo de novo?</h1>
+    <span className={styles.detail}>Hoje, aceitas casar comigo de novo?</span></h1>
 }
 
 export default Home;
